b2b0fbc8140381c9f0725b2631b97e244b832d72
Aun Syed Shah <aunsyedsyed@gmail.com>
2020-10-18 10:08:32 +0500

    partial registration form in python

--- share_now/forms.py
+++ share_now/forms.py
@@ -1,10 +1,18 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired, Length
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Length, Email, EqualTo
+
 
 class RegistrationForm(FlaskForm):
     """
     Registration Form
     """
-    username = StringField("Username", validators=[DataRequired(), Length(4, 8)])
-    submit = SubmitField("Submit")
+    username = StringField("Username", validators=[
+                           DataRequired(message="Username required"), Length(min=3, max=15)])
+    email = StringField("Email", validators=[DataRequired(
+        message="Please enter a valid email address"), Email()])
+    password = PasswordField("Password", validators=[
+                             DataRequired("Password must be between 8 to 15 characters"), Length(min=8, max=15)])
+    confirm_password = PasswordField("Confirm Password", validators=[
+                                     DataRequired(), EqualTo("password", message="Password must match")])
+    submit = SubmitField("Register")
